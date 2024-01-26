41638438b83fbeab6effaf255b16595205acb7a9
Rahat Hossain <webmaaz@gmail.com>
2024-01-26 14:20:45 +0600

    update

--- js/Home/per.js
+++ js/Home/per.js
@@ -11,7 +11,7 @@ const loader = document.getElementById('preloader')
                 mainStyle.href = 'css/style.css';
                 document.head.appendChild(mainStyle);
 
-            }, 2000);
+            }, 1900);
             setTimeout(() => {
 
 
@@ -45,7 +45,7 @@ const loader = document.getElementById('preloader')
 
                 jQuery(".left-imgbox").theiaStickySidebar({ additionalMarginTop: 80 }), jQuery(".right-imgbox").theiaStickySidebar({ additionalMarginTop: 80 })
 
-            }, 2300);
+            }, 2500);
 
             setTimeout(() => {
 
