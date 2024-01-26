da6ca002fb0ebf9f62aed97c12d798e74bdd4e3e
Rahat Hossain <webmaaz@gmail.com>
2024-01-26 14:38:20 +0600

    update

--- js/Home/per.js
+++ js/Home/per.js
@@ -64,5 +64,5 @@ const loader = document.getElementById('preloader')
 
             setTimeout(() => {
                 !function (e, t, n) { var s, o = e.getElementsByTagName(t)[0]; e.getElementById(n) || ((s = e.createElement(t)).id = n, s.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1365407963556488", s.setAttribute('defer', null), o.parentNode.insertBefore(s, o)) }(document, "script", "facebook-jssdk")
-            }, 10000)
+            }, 6000)
         };
