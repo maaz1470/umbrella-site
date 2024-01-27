9e9f0224357a9f69019e7665f26dea2bf0cb8e8b
Rahat Hossain <webmaaz@gmail.com>
2024-01-27 12:53:04 +0600

    update

--- js/Home/per.js
+++ js/Home/per.js
@@ -1,76 +1,78 @@
 const loader = document.getElementById('preloader')
 
-        window.onload = function () {
-            // const temp_data = document.getElementById('body').innerHTML
-            // document.getElementById('body').innerHTML = '';
-            // Main Css
-            // const mainStyle = document.createElement('link');
-            // mainStyle.rel = 'stylesheet';
-            // mainStyle.href = 'css/style.css';
-            // document.head.appendChild(mainStyle);
+window.onload = function () {
+    // const temp_data = document.getElementById('body').innerHTML
+    // document.getElementById('body').innerHTML = '';
+    // Main Css
+    // const mainStyle = document.createElement('link');
+    // mainStyle.rel = 'stylesheet';
+    // mainStyle.href = 'css/style.css';
+    // document.head.appendChild(mainStyle);
 
-            
+    setTimeout(() => {
+        // Common Script Load Here
+        const c_script = document.createElement('script');
+        c_script.src = 'js/common_scripts_min.js';
+        document.body.appendChild(c_script);
+    }, 2900)
 
-            setTimeout(() => {
+    setTimeout(() => {
 
 
-                 // Common Script Load Here
-            const c_script = document.createElement('script');
-            c_script.src = 'js/common_scripts_min.js';
-            document.body.appendChild(c_script);
 
-                // Vendor Css Load Here
-                var stylesheet = document.createElement('link');
-                stylesheet.rel = 'stylesheet';
-                stylesheet.href = 'css/vendors.css';
-                document.head.appendChild(stylesheet);
 
-                // Notify Css Load Here
-                const notify = document.createElement('script');
-                notify.src = 'js/notify_func.js';
-                document.body.appendChild(notify);
+        // Vendor Css Load Here
+        var stylesheet = document.createElement('link');
+        stylesheet.rel = 'stylesheet';
+        stylesheet.href = 'css/vendors.css';
+        document.head.appendChild(stylesheet);
 
-                // Content
-                // document.getElementById('body').innerHTML = temp_data;
+        // Notify Css Load Here
+        const notify = document.createElement('script');
+        notify.src = 'js/notify_func.js';
+        document.body.appendChild(notify);
 
-                // Stiky js Load Here
-                const sticky = document.createElement('script');
-                sticky.src = 'js/theia-sticky-sidebar.js';
-                document.body.appendChild(sticky);
+        // Content
+        // document.getElementById('body').innerHTML = temp_data;
 
-                // function js Load Here
-                const script_function = document.createElement('script');
-                script_function.src = 'js/functions.js';
-                document.body.appendChild(script_function);
+        // Stiky js Load Here
+        const sticky = document.createElement('script');
+        sticky.src = 'js/theia-sticky-sidebar.js';
+        document.body.appendChild(sticky);
 
-               
+        // function js Load Here
+        const script_function = document.createElement('script');
+        script_function.src = 'js/functions.js';
+        document.body.appendChild(script_function);
 
-                // Loading
-                // loader.style.display = 'none';
 
-                jQuery(".left-imgbox").theiaStickySidebar({ additionalMarginTop: 80 }), jQuery(".right-imgbox").theiaStickySidebar({ additionalMarginTop: 80 })
 
-            }, 3000);
+        // Loading
+        // loader.style.display = 'none';
 
-            setTimeout(() => {
+        jQuery(".left-imgbox").theiaStickySidebar({ additionalMarginTop: 80 }), jQuery(".right-imgbox").theiaStickySidebar({ additionalMarginTop: 80 })
 
-                // Google Font Load
-                var gFont = document.createElement('link');
-                gFont.rel = 'stylesheet';
-                gFont.href = 'https://fonts.googleapis.com/css2?family=Gochi+Hand&family=Montserrat:wght@300;400;500;600;700&display=swap';
-                document.head.appendChild(gFont);
+    }, 3000);
 
-                // Google Analytics Load Here
-                const googleTag = document.createElement('script');
-                googleTag.src = 'https://www.googletagmanager.com/gtag/js?id=UA-17468101-1';
-                document.body.appendChild(googleTag);
+    setTimeout(() => {
 
-                // Google Gtag Activation
-                function gtag() { dataLayer.push(arguments) } window.dataLayer = window.dataLayer || [], gtag("js", new Date), gtag("config", "UA-17468101-1")
+        // Google Font Load
+        var gFont = document.createElement('link');
+        gFont.rel = 'stylesheet';
+        gFont.href = 'https://fonts.googleapis.com/css2?family=Gochi+Hand&family=Montserrat:wght@300;400;500;600;700&display=swap';
+        document.head.appendChild(gFont);
 
-            }, 3500)
+        // Google Analytics Load Here
+        const googleTag = document.createElement('script');
+        googleTag.src = 'https://www.googletagmanager.com/gtag/js?id=UA-17468101-1';
+        document.body.appendChild(googleTag);
 
-            setTimeout(() => {
-                !function (e, t, n) { var s, o = e.getElementsByTagName(t)[0]; e.getElementById(n) || ((s = e.createElement(t)).id = n, s.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1365407963556488", s.setAttribute('defer', null), o.parentNode.insertBefore(s, o)) }(document, "script", "facebook-jssdk")
-            }, 4000)
-        };
+        // Google Gtag Activation
+        function gtag() { dataLayer.push(arguments) } window.dataLayer = window.dataLayer || [], gtag("js", new Date), gtag("config", "UA-17468101-1")
+
+    }, 3500)
+
+    setTimeout(() => {
+        !function (e, t, n) { var s, o = e.getElementsByTagName(t)[0]; e.getElementById(n) || ((s = e.createElement(t)).id = n, s.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1365407963556488", s.setAttribute('defer', null), o.parentNode.insertBefore(s, o)) }(document, "script", "facebook-jssdk")
+    }, 4000)
+};
