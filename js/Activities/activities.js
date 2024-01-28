46847f4421c812b0524d26d2aa18f76e3f71213c
Rahat Hossain <webmaaz@gmail.com>
2024-01-28 12:16:12 +0600

    update

--- js/Activities/activities.js
+++ js/Activities/activities.js
@@ -1,73 +1,79 @@
-const loader = document.getElementById('preloader')
+// const loader = document.getElementById('preloader')
 
-        window.onload = function () {
-            const temp_data = document.getElementById('body').innerHTML
-            document.getElementById('body').innerHTML = '';
-            setTimeout(() => {
+window.onload = function () {
+    // const temp_data = document.getElementById('body').innerHTML
+    // document.getElementById('body').innerHTML = '';
+    setTimeout(() => {
 
-                // Main Css
-                const mainStyle = document.createElement('link');
-                mainStyle.rel = 'stylesheet';
-                mainStyle.href = 'css/style.css';
-                document.head.appendChild(mainStyle);
+        // Vendor Css Load Here
+        var stylesheet = document.createElement('link');
+        stylesheet.rel = 'stylesheet';
+        stylesheet.href = 'css/vendors.css';
+        document.head.appendChild(stylesheet);
 
-            }, 2900);
-            setTimeout(() => {
+    }, 100);
 
-
-                // Vendor Css Load Here
-                var stylesheet = document.createElement('link');
-                stylesheet.rel = 'stylesheet';
-                stylesheet.href = 'css/vendors.css';
-                document.head.appendChild(stylesheet);
-
-                // Notify Css Load Here
-                const notify = document.createElement('script');
-                notify.src = 'js/notify_func.js';
-                document.body.appendChild(notify);
-
-                // Content
-                document.getElementById('body').innerHTML = temp_data;
-
-                // Stiky js Load Here
-                const sticky = document.createElement('script');
-                sticky.src = 'js/theia-sticky-sidebar.js';
-                document.body.appendChild(sticky);
-
-                // function js Load Here
-                const script_function = document.createElement('script');
-                script_function.src = 'js/functions.js';
-                document.body.appendChild(script_function);
+    setTimeout(() => {
+        // function js Load Here
+        const script_function = document.createElement('script');
+        script_function.src = 'js/functions.js';
+        document.body.appendChild(script_function);
+    }, 500)
+    setTimeout(() => {
 
 
-                // Loading
-                loader.style.display = 'none';
 
-                jQuery(".left-imgbox").theiaStickySidebar({ additionalMarginTop: 80 }), jQuery(".right-imgbox").theiaStickySidebar({ additionalMarginTop: 80 })
 
-            }, 3000);
+        // Notify Css Load Here
+        const notify = document.createElement('script');
+        notify.src = 'js/notify_func.js';
+        document.body.appendChild(notify);
 
-            setTimeout(() => {
+        // Content
+        // document.getElementById('body').innerHTML = temp_data;
 
-                // Google Font Load
-                var gFont = document.createElement('link');
-                gFont.rel = 'stylesheet';
-                gFont.href = 'https://fonts.googleapis.com/css2?family=Gochi+Hand&family=Montserrat:wght@300;400;500;600;700&display=swap';
-                document.head.appendChild(gFont);
+        // Stiky js Load Here
+        const sticky = document.createElement('script');
+        sticky.src = 'js/theia-sticky-sidebar.js';
+        document.body.appendChild(sticky);
 
-                // Google Analytics Load Here
-                const gtag = document.createElement('script');
-                gtag.src = 'https://www.googletagmanager.com/gtag/js?id=UA-17468101-1';
-                document.body.appendChild(gtag);
 
-            }, 6000)
 
-            setTimeout(() => {
-                // Recaptcha
-                const recaptcha = document.createElement('script');
-                recaptcha.src = 'https://www.google.com/recaptcha/api.js';
-                document.head.appendChild(recaptcha);
 
-                !function (e, t, n) { var s, o = e.getElementsByTagName(t)[0]; e.getElementById(n) || ((s = e.createElement(t)).id = n, s.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1365407963556488", s.setAttribute('defer', null), o.parentNode.insertBefore(s, o)) }(document, "script", "facebook-jssdk")
-            }, 10000)
-        };
+        // Loading
+        // loader.style.display = 'none';
+
+        jQuery(".left-imgbox").theiaStickySidebar({ additionalMarginTop: 80 }), jQuery(".right-imgbox").theiaStickySidebar({ additionalMarginTop: 80 })
+
+    }, 3000);
+
+    setTimeout(() => {
+
+        // Google Font Load
+        var gFont = document.createElement('link');
+        gFont.rel = 'stylesheet';
+        gFont.href = 'https://fonts.googleapis.com/css2?family=Gochi+Hand&family=Montserrat:wght@300;400;500;600;700&display=swap';
+        document.head.appendChild(gFont);
+
+
+    }, 4000)
+
+    setTimeout(() => {
+
+        // Google Analytics Load Here
+        const gtag = document.createElement('script');
+        gtag.src = 'https://www.googletagmanager.com/gtag/js?id=UA-17468101-1';
+        document.body.appendChild(gtag);
+
+        
+        // Google Gtag Activation
+        function gtag() { dataLayer.push(arguments) } window.dataLayer = window.dataLayer || [], gtag("js", new Date), gtag("config", "UA-17468101-1")
+
+        // Recaptcha
+        const recaptcha = document.createElement('script');
+        recaptcha.src = 'https://www.google.com/recaptcha/api.js';
+        document.head.appendChild(recaptcha);
+
+        !function (e, t, n) { var s, o = e.getElementsByTagName(t)[0]; e.getElementById(n) || ((s = e.createElement(t)).id = n, s.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1365407963556488", s.setAttribute('defer', null), o.parentNode.insertBefore(s, o)) }(document, "script", "facebook-jssdk")
+    }, 4500)
+};
--- js/Contact/b_contact.js
+++ js/Contact/b_contact.js
@@ -64,7 +64,7 @@ window.onload = function () {
         // Google Gtag Activation
         function gtag() { dataLayer.push(arguments) } window.dataLayer = window.dataLayer || [], gtag("js", new Date), gtag("config", "UA-17468101-1")
 
-    }, 6000)
+    }, 3500)
 
     setTimeout(() => {
 
@@ -79,5 +79,5 @@ window.onload = function () {
 
 
         !function (e, t, n) { var s, o = e.getElementsByTagName(t)[0]; e.getElementById(n) || ((s = e.createElement(t)).id = n, s.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1365407963556488", s.setAttribute('defer', null), o.parentNode.insertBefore(s, o)) }(document, "script", "facebook-jssdk")
-    }, 7000)
+    }, 4000)
 };
