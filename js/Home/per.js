6226d8eb50894b8cb1faa3b4cf4d0d5c45ec8648
Rahat Hossain <webmaaz@gmail.com>
2024-01-28 13:41:37 +0600

    update

--- js/Home/per.js
+++ js/Home/per.js
@@ -67,7 +67,7 @@ window.onload = function () {
         gFont.href = 'https://fonts.googleapis.com/css2?family=Gochi+Hand&family=Montserrat:wght@300;400;500;600;700&display=swap';
         document.head.appendChild(gFont);
 
-    }, 4200)
+    }, 3000)
 
     setTimeout(() => {
 
